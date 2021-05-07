5adc70c9eb733364de9c04f4fdefee4b205844f3
Sreenivassreee <ksreenivascse@gmail.com>
2021-05-07 23:04:41 +0530

    normal quotes showing

--- In-APP purchase App/TableViewController.swift
+++ In-APP purchase App/TableViewController.swift
@@ -6,82 +6,68 @@
 //
 
 import UIKit
+import StoreKit
 
 class TableViewController: UITableViewController {
-    var normalQuetes=["HI","Bye"]
+    var productID="com.stevebrains.In-APP-purchase-App"
+    var normalQuetes=["Love all, trust a few, do wrong to none.",
+                      "You call it madness, but I call it love.",
+                      "We can only learn to love by loving.",
+                      "A life lived in love will never be dull.",
+                      "Life is the flower for which love is the honey.",
+                      "All you need is love.",
+                      "True love stories never have endings."]
     let premiumQuetes=["HI ,man","Bye man"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
        
     }
+  
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+ 
+        return normalQuetes.count+1
     }
+    @IBAction func RestoredPressed(_ sender: UIBarButtonItem) {
+    }
+    
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QCell", for: indexPath)
+      
+        if indexPath.row<normalQuetes.count{
+            cell.textLabel?.text = normalQuetes[indexPath.row]
+            cell.textLabel?.numberOfLines=0
+        }else{
+            cell.textLabel?.text="Get More"
+            cell.textLabel?.textColor=#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            cell.accessoryType =    .disclosureIndicator
+            
+        }
+       
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row==normalQuetes.count{
+            print("Buy")
+            buyQuotes()
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    
+    func buyQuotes(){
+        if SKPaymentQueue.canMakePayments(){
+            let payment = SKMutablePayment()
+            payment.productIdentifier=productID
+            SKPaymentQueue.default().add(payment)
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
